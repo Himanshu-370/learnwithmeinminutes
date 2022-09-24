a40a6d1e2f42e2065176e1658f4f9f6ebecfc9cc
Himanshu Singh <himanshuich20@gmail.com>
2022-09-24 19:05:57 +0530

    adding files

---
+++ components/Header.jsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+function Header() {
+  return (
+    <header>
+      <div className="container">
+        <Link href="/">
+          <h1>Learn with me in minutes</h1>
+        </Link>
+
+        <p>
+          Take a whirlwind tour of your next favorite language.
+          Community-driven!
+        </p>
+
+        <form action="#">
+          <label className="switch">
+            <input type="checkbox" />
+            <span onClick={() => themeToggler()} className="slider"></span>
+          </label>
+        </form>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
